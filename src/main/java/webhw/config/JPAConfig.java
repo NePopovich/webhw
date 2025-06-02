bfc50f45c6d63c5b3198ef5ee9c7b7b950250e04
Aleksey Larkov <djalekseeff@gmail.com>
2025-06-02 20:13:21 +0300

    Добавлен файл properties, реализовано соединение через HikariCP

--- src/main/java/webhw/config/JPAConfig.java
+++ src/main/java/webhw/config/JPAConfig.java
@@ -1,8 +1,15 @@
 package webhw.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -16,16 +23,20 @@ import javax.sql.DataSource;
 @Configuration
 @EnableJpaRepositories(basePackages = "webhw")
 @EnableTransactionManagement
+@PropertySource("classpath:dbconfig.properties")
 public class JPAConfig {
+
+    @Autowired
+    private Environment env;
 
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource =new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
-        return dataSource;
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName(env.getProperty("db.driver"));
+        hikariConfig.setJdbcUrl(env.getProperty("db.url"));
+        hikariConfig.setUsername(env.getProperty("db.user"));
+        hikariConfig.setPassword(env.getProperty("db.password"));
+        return new HikariDataSource(hikariConfig);
     }
 
     @Bean
